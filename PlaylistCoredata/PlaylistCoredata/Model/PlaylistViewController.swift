3f55d142e22fe76a5dcad6931ccadbf056434547
Matthew Werdean <matthewwerdean@Matthews-MacBook-Pro.local>
2020-03-04 14:10:49 -0700

    Storyboard implemented

--- PlaylistCoredata/PlaylistCoredata/Model/PlaylistViewController.swift
+++ PlaylistCoredata/PlaylistCoredata/Model/PlaylistViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class PlaylistViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var playlistNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
